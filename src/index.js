a447e1073e30eabee4cd0eb25f5283bdab50162d
kiet.nguyen <kiet.nguyen@tatmart.com>
2023-05-20 22:26:43 +0700

    init route and controller

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.json());
+
 app.use(morgan('combined'))
 
 //template engine
@@ -14,11 +23,9 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-// console.log(path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
